10801cc924665feb587928ab2dad57f4310805ff
Alwin Jose <valwinjose007@gmail.com>
2025-03-29 20:48:31 +0530

    initial commit

--- src/components/Home/ProductSlider/Product.jsx
+++ src/components/Home/ProductSlider/Product.jsx
@@ -32,7 +32,7 @@ const Product = (props) => {
             {/* <!-- image & product title --> */}
             <Link to={`/product/${_id}`} className="flex flex-col items-center text-center group">
                 <div className="w-36 h-36">
-                    <img draggable="false" className="w-full h-full object-contain" src={images[0].url} alt={name} />
+                    <img draggable="false" className="w-full h-full object-contain" src={images[0]?.url} alt={name} />
                 </div>
                 <h2 className="text-sm mt-4 group-hover:text-primary-blue">{name.length > 50 ? `${name.substring(0, 50)}...` : name}</h2>
             </Link>
@@ -43,14 +43,14 @@ const Product = (props) => {
                 {/* <!-- rating badge --> */}
                 <span className="text-sm text-gray-500 font-medium flex gap-2 items-center">
                     <span className="text-xs px-1.5 py-0.5 bg-primary-green rounded-sm text-white flex items-center gap-0.5">{ratings.toFixed(1)} <StarIcon sx={{ fontSize: "14px" }} /></span>
-                    <span>({numOfReviews.toLocaleString()})</span>
+                    <span>({numOfReviews?.toLocaleString()})</span>
                 </span>
                 {/* <!-- rating badge --> */}
 
                 {/* <!-- price container --> */}
                 <div className="flex items-center gap-1.5 text-md font-medium">
-                    <span>₹{price.toLocaleString()}</span>
-                    <span className="text-gray-500 line-through text-xs">₹{cuttedPrice.toLocaleString()}</span>
+                    <span>₹{price?.toLocaleString()}</span>
+                    <span className="text-gray-500 line-through text-xs">₹{cuttedPrice?.toLocaleString()}</span>
                     <span className="text-xs text-primary-green">{getDiscount(price, cuttedPrice)}%&nbsp;off</span>
                 </div>
                 {/* <!-- price container --> */}
