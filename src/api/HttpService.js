d0094ae7802f6727b619820293aa8614d1caa102
Alwin Jose <valwinjose007@gmail.com>
2025-06-24 00:07:14 +0530

    baseurl update

--- src/api/HttpService.js
+++ src/api/HttpService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base config
 const axiosInstance = axios.create({
-    baseURL: process.env.BACKEND_URL || 'http://localhost:4000',
+    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000',
     headers: {
         'Content-Type': 'application/json',
     },
