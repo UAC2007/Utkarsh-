e78674899e20e196bc674df1b3bfc4d26a51294b
Bino <binob831@gmail.com>
2025-06-24 22:12:38 +0530

    Update URi

--- src/api/HttpService.js
+++ src/api/HttpService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base config
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000',
+    baseURL: process.env.REACT_APP_BACKEND_URL,
     headers: {
         'Content-Type': 'application/json',
     },
