df5399800e6de38199047c11beb7ee0b48e8eda7
Alwin Jose <valwinjose007@gmail.com>
2025-07-01 10:24:34 +0530

    axios token not passing first time fix.

--- src/api/HttpService.js
+++ src/api/HttpService.js
@@ -5,10 +5,23 @@ const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:4000",
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token') || ''}`
     },
     withCredentials: true
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    } else {
+      delete config.headers['Authorization']; // Remove if not set
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 
 export const getCall = (url, config = {}) => axiosInstance.get(url, config);
 
